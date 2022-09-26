3a85e493bf502c6ce040d1e4344f4c1bb1334463
Tim Robbins <86779745+Legatus727@users.noreply.github.com>
2022-09-26 09:15:16 -0700

    Basic Class setup

--- src/com/jeopardy/client/GameClient.java
+++ src/com/jeopardy/client/GameClient.java
@@ -1,4 +1,70 @@
 package com.jeopardy.client;
 
 class GameClient {
+    public static void main(String[] args) {
+
+        // setup()
+        // welcomeScreen()
+        // playerSetup()
+        // playSJ()
+        // playDJ()
+        // playFJ()
+        // displayResults()
+
+    }
+
+    private void setup() {
+        // new gameboard
+        // populate gameboard with questions
+        // maybe in main?
+    }
+
+    private void welcomeScreen() {
+        // splast art welcome
+        // maybe transitions with multiple welcomes
+
+    }
+
+    private void playerSetup() {
+        // Jeopardy banner
+        // Display rules
+        // Prompt for user name
+
+    }
+
+    private void playSJ() {
+        // SJ banner
+        // display board
+        // loop
+        //     prompt for category
+        //     prompt for value
+        //     gameBoard methods
+        // End of SJ banner
+
+    }
+
+    private void playDJ() {
+        // DJ banner
+        // display board
+        // loop
+        //     prompt for category
+        //     prompt for value
+        //     gameBoard methods
+        // End of DJ banner
+
+    }
+
+    private void playFJ() {
+        // FJ banner
+        // display question
+        // prompt for answer
+        // display answer and player question result
+
+    }
+
+    private void displayResults() {
+        // End of game banner
+        // Player name with score
+
+    }
 }
