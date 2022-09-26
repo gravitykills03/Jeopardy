46578c73523d9fce80f40537128326b2ec5fb2eb
Martisha <martishalewis@yahoo.com>
2022-09-26 15:46:06 -0500

    Generic Player class

--- src/com/jeopardy/Player.java
+++ src/com/jeopardy/Player.java
@@ -1,4 +1,37 @@
 package com.jeopardy;
 
-class Player {
+public class Player {
+    private String name;
+    private int money;
+
+    public Player(String name, int money) {
+        this.name = name;
+        this.money = money;
+    }
+
+    public void addMoney(int value) {
+        this.money += money;
+    }
+
+    public void subtractMoney(int value) {
+       this.money -= money;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    @Override
+    public String toString() {
+        return "Player name: " + name +
+                ", money: " + money;
+    }
 }
